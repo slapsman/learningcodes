72fe062a1d8b43803d33337c98b2ad6bd0c1b964
slapsekili <selim1lapsekili@gmail.com>
2025-07-21 13:47:36 +0300

    ilk commit

---
+++ cift_sayi.py
@@ -0,0 +1,6 @@
+print("Tekmi Çiftmi hesapliyicisina Hoş Geldiniz ")
+girilen_sayi=int(input("sayi giriniz ")) 
+if girilen_sayi % 2==0:
+    print("girdiğiniz sayi çift")
+else:
+    print("girdiğiz sayi tek")
